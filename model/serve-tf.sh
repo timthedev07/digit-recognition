86fda53f9b8763856eccc93ab7615ad5ef734afa
Tim <harvard90873@gmail.com>
2022-07-11 22:22:51 +0200

    use dockerfile

--- model/serve-tf.sh
+++ model/serve-tf.sh
@@ -1,4 +1,5 @@
-# docker pull tensorflow/serving
-docker run -p 8501:8501 \
+#!/bin/bash
+
+docker run -p 8501:"${PORT}" \
   --mount type=bind,source="$(pwd)/build",target=/models/digit-recognition \
   -e MODEL_NAME=digit-recognition -t tensorflow/serving
