9d5b0d7d14a56a7fa7cb824e9071b72ce4841270
Tim <harvard90873@gmail.com>
2022-07-12 13:47:03 +0200

    fix helper

--- client/src/utils/canvasImgConverter.ts
+++ client/src/utils/canvasImgConverter.ts
@@ -5,13 +5,15 @@ export const imgDataTo3DArr = (data: Uint8ClampedArray, width: number) => {
   for (let i = 0, n = data.length; i < n; i += 4) {
     const pixelInRGBA = [data[i], data[i + 1], data[i + 2], data[i + 3]];
 
-    if (row.length > width) {
+    if (row.length >= width) {
       rows.push(row);
       row = [];
     } else {
       row.push(pixelInRGBA);
     }
   }
+
+  rows.push(row);
 
   return rows;
 };
