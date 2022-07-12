a11aa364d88acacffcfa0677e99773da20d0f91a
Tim <harvard90873@gmail.com>
2022-07-12 13:43:35 +0200

    add helper for processing canvas image data

---
+++ client/src/utils/canvasImgConverter.ts
@@ -0,0 +1,17 @@
+export const imgDataTo3DArr = (data: Uint8ClampedArray, width: number) => {
+  let row: number[][] = [];
+  const rows: number[][][] = [];
+
+  for (let i = 0, n = data.length; i < n; i += 4) {
+    const pixelInRGBA = [data[i], data[i + 1], data[i + 2], data[i + 3]];
+
+    if (row.length > width) {
+      rows.push(row);
+      row = [];
+    } else {
+      row.push(pixelInRGBA);
+    }
+  }
+
+  return rows;
+};
